0779fa2e01fdbbdefc416ed6ebc5d6601f3d8d8d
kainapa <68212816+kainapa@users.noreply.github.com>
2020-10-18 20:17:49 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace oop_6115261002_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=ice_cream";
+
+
+                string Query = "update ice_cream set p_ID='" + this.txtID.Text + "',p_Name ='" + this.txtName.Text + "'" + "' where p_Price'" + this.txtPrice.Text +  "';";
+
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataReader MyReader2;
+
+                MyConn2.Open();
+
+                MyReader2 = MyCommand2.ExecuteReader();
+
+                MessageBox.Show("Data Updated");
+
+                while (MyReader2.Read())
+                {
+
+
+
+                }
+
+                MyConn2.Close();//Connection closed here 
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+        private void butPDisplay_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=ice_cream";
+                string Query = "select * from ice_cream;";
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                MySqlDataAdapter MyAdapter = new MySqlDataAdapter();
+                MyAdapter.SelectCommand = MyCommand2;
+
+
+                DataTable dTable = new DataTable();
+
+
+                MyAdapter.Fill(dTable);
+                dataGridView1.DataSource = dTable;
+
+
+
+
+
+
+                // MyConn2.Close(); 
+
+
+            }
+
+
+            catch (Exception ex)
+            {
+
+
+
+
+
+
+                MessageBox.Show(ex.Message);
+
+
+            }
+
+        }
+
+        private void butPAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=ice_cream";
+
+                string Query = "insert into ice_cream(p_ID,p_Name,p_Price) " +
+                    "values('" + this.txtID.Text + "','" + this.txtName.Text + "','" + this.txtPrice.Text + "');";
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataReader MyReader2;
+
+                MyConn2.Open();
+
+                MyReader2 = MyCommand2.ExecuteReader();
+
+                MessageBox.Show("Save Data");
+
+                while (MyReader2.Read())
+                {
+
+
+
+                }
+
+                MyConn2.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+
+        private void butPDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string MyConnection2 = "datasource=localhost;port=3306;username=root;password=;database=ice_cream";
+
+                string Query = "delete from ice_cream where p_ID'" + this.txtID.Text + "',p_Name'" + this.txtName.Text + "',p_Price'" + this.txtPrice.Text + "';";
+
+                MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+
+                MySqlDataReader MyReader2;
+
+                MyConn2.Open();
+
+                MyReader2 = MyCommand2.ExecuteReader();
+
+                MessageBox.Show("Data Deleted");
+
+                while (MyReader2.Read())
+                {
+
+
+
+                }
+
+                MyConn2.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+
+
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+        }
+
+        private void butPNext_Click(object sender, EventArgs e)
+        {
+            {
+                Form2 f2 = new Form2();
+                f2.ShowDialog();
+            }
+
+        }
+    }
+}
